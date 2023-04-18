2a5b5d7ee268b9f4a8211253923a9b45cb79a569
Zack <zachary.berkley@gmail.com>
2023-04-18 08:56:15 -0400

    blank postcard rendering

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,23 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Categories, PostCard, PostWidget } from "../components";
-
-const posts = [
-  { title: "Test Post", excerpt: "Practice excerpt" },
-  { title: "Test Post v2", excerpt: "Practice excerpt, but better" },
-];
+import { getPosts } from "../services";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
-    <div className="container mx-auto px-10 mb-8 bg-gray-300">
+    <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>CBT Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => (
+          {posts.map((post) => 
             <PostCard post={post} key={post.title} />
-          ))}
+          
+          )}
         </div>
 
         <div className="lg:col-span-4 col-span-1">
@@ -32,4 +29,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
 }
