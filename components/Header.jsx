8eda907a321487deedbf0177fe059ce16eabc472
Zack <zachary.berkley@gmail.com>
2023-04-19 09:21:24 -0400

    components for slug added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,24 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
+
+import { getCategories } from "@/services";
 
 import Link from "next/link";
 
-const categories = [
-  { name: "Mental Health", slug: "mental-health" },
-  { name: "Daily Affirmation", slug: "daily-affirmation" },
-];
-
 const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
+  
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="border-b w-full inline-block border-blue-400 py-8">
+      <div className="border-b w-full inline-block border-slate-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="cursor-pointer font-bold text-4xl text-white">
+            <span className="cursor-pointer font-bold text-4xl text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
               CBT Blog
             </span>
           </Link>
@@ -22,7 +27,7 @@ const Header = () => {
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
+              <span className="md:float-right mt-2 align-middle text-white drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)] ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
