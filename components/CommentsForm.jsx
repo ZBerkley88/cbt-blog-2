05e3f178107755f02a69582ede2db052da5cbb8f
Zack <zachary.berkley@gmail.com>
2023-04-25 21:43:56 -0400

    added a working comment form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,11 +1,127 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 
-const CommentsForm = () => {
+import { submitComment } from "@/services";
+
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, stateShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
+  useEffect(() => {
+    nameEl.current.value = window.localStorage.getItem("name");
+    emailEl.current.value = window.localStorage.getItem("email");
+  });
+
+  const handleCommentSubmission = () => {
+    setError(false);
+
+    const { value: comment } = commentEl.current;
+    const { value: name } = nameEl.current;
+    const { value: email } = emailEl.current;
+    const { checked: storeData } = storeDataEl.current;
+
+    if (!comment || !name || !email) {
+      setError(true);
+      return;
+    }
+    const commentObj = {
+      name,
+      email,
+      comment,
+      slug,
+    };
+
+    if (storeData) {
+      window.localStorage.setItem("name", name);
+      window.localStorage.setItem("email", email);
+    } else {
+      window.localStorage.removeItem("name", name);
+      window.localStorage.removeItem("email", email);
+    }
+
+    submitComment(commentObj).then((res) => {
+      stateShowSuccessMessage(true);
+
+      setTimeout(() => {
+        stateShowSuccessMessage(false);
+      }, 3000);
+    });
+  };
+
   return (
-    <div>
-      <h1>
-        CommentsForm
-      </h1>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8 mr-8">
+      <h3 className="text-xl mb-b font-semibold border-b pb-4">Comment Form</h3>
+
+      {/* ================= COMMENT ================= */}
+      <div className="grid grid-cols-1 gap-4 mb-4 mt-8">
+        <textarea
+          ref={commentEl}
+          className="p-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Comment"
+          name="comment"
+        />
+      </div>
+
+      {/* ================= NAME ================= */}
+      <div className="grid grid-cols-1 gap-4 mb-4 lg:grid-cols-2">
+        <input
+          type="text"
+          ref={nameEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Name"
+          name="name"
+        />
+
+        {/* ================= EMAIL ================= */}
+        <input
+          type="text"
+          ref={emailEl}
+          className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          placeholder="Email"
+          name="email"
+        />
+      </div>
+
+      <div className="gap-4 mb-4">
+        <input
+          ref={storeDataEl}
+          type="checkbox"
+          id="storeData"
+          name="storeData"
+          value="true"
+        />
+        <label
+          className="text-gray-500 cursor-pointer ml-2"
+          htmlFor="storeData"
+        >
+          Save my e-mail
+        </label>
+      </div>
+
+      {/* ================= ERROR ================= */}
+      {error && <p className="text-xs text-red-500">All fields are required</p>}
+      <div className="mt-8">
+        {/* ================= BUTTON ================= */}
+        <button
+          type="button"
+          onClick={handleCommentSubmission}
+          className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg rounded-full text-white px-8 py-3 cursor-pointer"
+        >
+          Post Comment
+        </button>
+
+        {/* ================= SUCCESS ================= */}
+
+        {showSuccessMessage && (
+          <span className="text-xl float-right font-semibold mt-3 text-green 500">
+            Comment submitted for review!
+          </span>
+        )}
+      </div>
     </div>
   );
 };
